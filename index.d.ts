89863dea76c4dd68e7c32bba80ed8a37fae8e01a
lepzulnag <gindre.matthieu@gmail.com>
2021-01-23 12:36:35 +0100

    First commit

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export declare function start(testFunction: Function): void
+export declare function stage(name: string): void
+export declare function test(condition: boolean, description?: string): void
