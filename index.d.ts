072c08b1f01fe9df84d8f9699076b324501f79d6
lepzulnag <gindre.matthieu@gmail.com>
2021-01-23 13:18:27 +0100

    Update typescript

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 export declare function start(testFunction: Function): void
 export declare function stage(name: string): void
 export declare function test(condition: boolean, description?: string): void
+export declare function same(a: any, b: any, description?: string): void
